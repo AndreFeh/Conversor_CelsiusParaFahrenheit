ad2ddb751fc40a165cf18541fe4fba09fbe30de4
AssassinAFS <andrefelipe.c.santos@gmail.com>
2022-03-13 00:07:21 -0300

    send reposit convertCforF

---
+++ CelsiusParaFahrenheit.java
@@ -0,0 +1,15 @@
+package git;
+import java.util.Scanner; 
+public class CelsiusParaFahrenheit {
+
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Qual a temperatura em C°: ");
+		float graus = scan.nextFloat();
+//		A base do Fahrenheit é 32 = 0 Graus - entao...
+		float conversaoF = (graus * 5/9) + 32;
+		
+			System.out.println("Temperatura em Graus(C°): " + graus + "\n" + "Convertida em Fahrenheit(F°): " + conversaoF);	
+	}
+}
